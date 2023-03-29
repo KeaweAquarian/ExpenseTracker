cf7a39095658ff173b5fefd7510089ad37c623dd
KeaweAquarian <keaweaquarian@gmail.com>
2023-03-29 07:32:18 -0400

    added region config

--- src/main/java/com/KeaweAquarian/ExpenseTracker/config/AmazonConfig.java
+++ src/main/java/com/KeaweAquarian/ExpenseTracker/config/AmazonConfig.java
@@ -18,6 +18,7 @@ public class AmazonConfig {
         );
         return AmazonS3ClientBuilder
                 .standard()
+                .withRegion("us-east-1")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
