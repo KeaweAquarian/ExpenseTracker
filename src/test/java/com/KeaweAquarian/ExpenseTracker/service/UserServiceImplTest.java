05b2dfd294d2a7bef07df7ba98fe35e3a0496e72
KeaweAquarian <keaweaquarian@gmail.com>
2023-05-28 21:14:18 -0400

    Testing UserSevice

---
+++ src/test/java/com/KeaweAquarian/ExpenseTracker/service/UserServiceImplTest.java
@@ -0,0 +1,74 @@
+package com.KeaweAquarian.ExpenseTracker.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void saveCategory() {
+    }
+
+    @Test
+    void saveExpense() {
+    }
+
+    @Test
+    void saveRole() {
+    }
+
+    @Test
+    void addRoleToUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getRoles() {
+    }
+
+    @Test
+    void testGetUser() {
+    }
+
+    @Test
+    void uploadUserProfileImage() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void downloadUserProfileImage() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
